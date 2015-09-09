425517542e12462f7ca11da4e402d35822024d3f
Freark van der Bos <safariman@gmail.com>
2015-09-09 14:14:24 +0200

    Added class to generate dh params

---
+++ manifests/dhparam.pp
@@ -0,0 +1,21 @@
+# Little class to generate and install a server generated dh parameter
+class nginx::dhparam (
+    $key_size = 2048,
+    $location = '/opt/ssl/dhparam.pem',
+    $config_location = '/etc/nginx/conf.d/dhparam.conf'
+) {
+
+    exec { 'generate dh param':
+        command => "/usr/bin/openssl dhparam -out ${location} ${key_size}",
+        creates => $location,
+    }
+
+    file { $config_location: 
+        content => "ssl_dhparam ${location};",
+        owner   => 'root',
+        group   => 'www-data',
+        require => Exec['generate dh param'],
+        notify  => Service['nginx'],
+    }
+
+}
