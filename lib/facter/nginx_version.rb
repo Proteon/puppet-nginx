67e202967267da2c8e07fb0d7bd0523c83743a59
N/A <N/A>
2019-02-06 11:38:11 +0100

    updated nginxversion fact as it was determing nginx version by version of nginx-common however for current version (1.15) we dont have a common with that version installed

--- lib/facter/nginx_version.rb
+++ lib/facter/nginx_version.rb
@@ -1,7 +1,7 @@
-# nginx_version.rb
+#nginx_version.rb
 
 Facter.add("nginxversion") do
 	setcode do
-		Facter::Util::Resolution.exec('/usr/bin/dpkg-query -W -f=\'${Version}\' nginx-common 2>/dev/null')
+		Facter::Util::Resolution.exec('/usr/bin/dpkg-query -W -f=\'${Version}\' nginx 2>/dev/null')
 	end
 end
